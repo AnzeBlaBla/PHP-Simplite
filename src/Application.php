ce094101c2218cb2530518be1c0fc0c170478a0c
AnzeBlaBla <anzemajblabla@gmail.com>
2023-12-03 10:19:55 +0100

    Hiding php errors if debug is off

--- src/Application.php
+++ src/Application.php
@@ -78,6 +78,15 @@ class Application
 
         $this->validateConfig($config);
 
+        // If debug is on, turn on error reporting
+        if ($config['debug']) {
+            error_reporting(E_ALL);
+            ini_set('display_errors', 1);
+        } else {
+            error_reporting(0);
+            ini_set('display_errors', 0);
+        }
+
         $this->config = $config;
 
         if (isset($this->config['db'])) {
