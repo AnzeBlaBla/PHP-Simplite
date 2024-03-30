971b30192a32c64e33e14a611de4afefb08d1cb6
AnzeBlaBla <anzemajblabla@gmail.com>
2024-03-30 10:22:35 +0100

    New COMPONENT_ID generator

--- src/Renderer.php
+++ src/Renderer.php
@@ -16,7 +16,60 @@ class Renderer
     
     static $components_root = null;
     static $component_index = 0; // Used for unique component IDs
+
+    /**
+     * Generates a unique component ID
+     * 
+     * @return string The component ID
+     */
+    private static function nextComponentId()
+    {
+        // Convert request time to hex
+        $time = dechex(time());
+        // Get last 6 characters
+        $time = substr($time, -6);
+        
+
+        return 'SIMPLITE_' . $time . self::$component_index++;
+    }
+
+
+    /**
+     * Renders a component and returns the HTML output
+     * 
+     * @param string $file The component file name
+     * @param array $data The data to pass to the component
+     * @return string The HTML output
+     */
     static function component($file, $data = []): string
+    {
+        return self::_renderComponent($file, $data)['output'];
+    }
+
+    /**
+     * Renders a component, echoing the HTML output and returning the COMPONENT_ID
+     * 
+     * @param string $file The component file name
+     * @param array $data The data to pass to the component
+     * @return string The COMPONENT_ID
+     */
+    static function componentEcho($file, $data = []): string
+    {
+        $rendered = self::_renderComponent($file, $data);
+        echo $rendered['output'];
+        return $rendered['COMPONENT_ID'];
+    }
+
+
+    /**
+     * Renders a component, returning it's rendered HTML and the COMPONENT_ID (Note: not meant to be called directly, use Renderer::component() instead)
+     * 
+     * @param string $file The component file name
+     * @param array $data The data to pass to the component
+     * 
+     * @return array An array containing the rendered HTML and the COMPONENT_ID
+     */
+    static function _renderComponent($file, $data = []): array
     {
         if (!self::$components_root) {
             self::$components_root = Application::getInstance()->getConfig('root_directory') . '/' . Application::getInstance()->getConfig('components_folder');
@@ -31,8 +84,7 @@ class Renderer
             $app = Application::getInstance();
 
             // Get component ID
-            $COMPONENT_ID = 'component_' . self::$component_index;
-            self::$component_index++;
+            $COMPONENT_ID = self::nextComponentId();
             
             ob_start();
             // Include component
@@ -41,9 +93,15 @@ class Renderer
             $output = ob_get_contents();
             ob_end_clean();
 
-            return $output;
+            return [
+                'output' => $output,
+                'COMPONENT_ID' => $COMPONENT_ID
+            ];
         } else {
-            return "ERROR: Component not found: " . $file;
+            return [
+                'output' => "ERROR: Component not found: " . $file,
+                'COMPONENT_ID' => null
+            ];
         }
     }
 }
